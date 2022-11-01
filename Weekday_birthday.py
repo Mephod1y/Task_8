43b719526872d44826e7ea3419d897417e26f3f9
Mephod1y <73156817+Mephod1y@users.noreply.github.com>
2022-11-01 11:50:12 +0200

    Update Weekday_birthday.py

--- Weekday_birthday.py
+++ Weekday_birthday.py
@@ -1,37 +1,38 @@
 from datetime import datetime,timedelta
-weekdays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
-users = {"Max": "2022-11-02", "Tanya": "2022-11-01", "Petro": "2022-10-30", "Stepan": "2022-10-29"}
-dates = {}
-# date = datetime(year=2022, month=10, day=30)
+users = [{"name": "Max", "birthday": datetime(year=2022, month=11, day=2)},
+         {"name": "Tanya", "birthday": datetime(year=2022, month=11, day=2)},
+         {"name":"Petro", "birthday": datetime(year=2022, month=10, day=30)},
+         {"name":"Stepan", "birthday": datetime(year=2022, month=11, day=5)}]
+dates = []
 date = datetime.now()
-if datetime.date(date).weekday() == 0:
-    for i in range(-2,5):
-        dates[weekdays[(datetime.date(date) + timedelta(i)).weekday()]] = f"{datetime.date(date) + timedelta(i)}"
-elif datetime.date(date).weekday() in range(1,5):
-    for i in range(1,8):
-        dates[weekdays[(datetime.date(date) + timedelta(i)).weekday()]] = f"{datetime.date(date) + timedelta(i)}"
-elif datetime.date(date).weekday() == 5:
-    for i in range(1, 7):
-        dates[weekdays[(datetime.date(date) + timedelta(i)).weekday()]] = f"{datetime.date(date) + timedelta(i)}"
-else:
-    for i in range(-1, 6):
-        dates[weekdays[(datetime.date(date) + timedelta(i)).weekday()]] = f"{datetime.date(date) + timedelta(i)}"
 
-res = {"Monday":[],"Tuesday":[],"Wednesday":[],"Thursday":[],"Friday":[],"Saturday":[],"Sunday":[]}
-for day,date in dates.items():
-    for name,birth in users.items():
-        if birth == date:
-            res[day].append(name)
-sun = res["Sunday"]
-sat = res["Saturday"]
-for n in sun:
-    res["Monday"].append(n)
-for n in sat:
-    res["Monday"].append(n)
-del res["Sunday"]
-del res["Saturday"]
-result = {}
-for key in res:
-    if len(res[key]) > 0:
-        result[key] = res[key]
-print(result)
+def get_birthday(users):
+    if datetime.date(date).weekday() == 0:
+        for i in range(-2, 5):
+            dates.append(f"{datetime.date(date + timedelta(i))}")
+    elif datetime.date(date).weekday() in range(1, 5):
+        for i in range(1, 8):
+            dates.append(f"{datetime.date(date + timedelta(i))}")
+    elif datetime.date(date).weekday() == 5:
+        for i in range(1, 7):
+            dates.append(f"{datetime.date(date + timedelta(i))}")
+    else:
+        for i in range(-1, 6):
+            dates.append(f"{datetime.date(date + timedelta(i))}")
+
+    res = {"Monday":[],"Tuesday":[],"Wednesday":[],"Thursday":[],"Friday":[],"Saturday":[],"Sunday":[]}
+
+    for user in users:
+        birth = datetime.date(datetime(year=user.get('birthday').year, month=user.get('birthday').month, day=user.get('birthday').day)).strftime("%Y-%m-%d")
+        if birth in dates:
+            weekday = datetime.date(datetime(year=user.get('birthday').year, month=user.get('birthday').month,
+                                             day=user.get('birthday').day)).strftime("%A")
+            if weekday in ['Saturday', 'Sunday']:
+                weekday = 'Monday'
+            res.get(weekday).append(user.get('name'))
+
+    for key, value in res.items():
+        if value:
+            print(f"{key}: {', '.join(value)}")
+
+print(get_birthday(users))
